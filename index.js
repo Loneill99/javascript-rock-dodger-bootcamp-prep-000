0d846cb7dfb441166b181eeeeea4bc796e762ffa
Linda <oneill.linda07@gmail.com>
2017-12-04 23:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,7 @@ var gameInterval = null
  * Be aware of what's above this line,
  * but all of your work should happen below.
  */
- function start() {
-  window.addEventListener('keydown', moveDodger)
 
-  START.style.display = 'none'
-
-  gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  }, 1000)
-}
 
 
 function checkCollision(rock) {
@@ -194,4 +186,13 @@ function positionToInteger(p) {
   return parseInt(p.split('px')[0]) || 0
 }
 
+ function start() {
+  window.addEventListener('keydown', moveDodger)
+
+  START.style.display = 'none'
+
+  gameInterval = setInterval(function() {
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
+  }, 1000)
+}
 
